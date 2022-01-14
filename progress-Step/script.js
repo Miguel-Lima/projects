3ea92a5062a271685a27a72e587aaa817c7610d7
Miguel Lima <miguellimaoliver97@gmail.com>
2022-01-14 20:08:03 -0300

    Add evento de clicks, prev e next

--- progress-Step/script.js
+++ progress-Step/script.js
@@ -1,4 +1,26 @@
 const progress = document.getElementById('progress');
-const prev = document.getElementById('prev')
-const next = document.getElementById('next')
-const circles = document.querySelectorAll('circle')
+const prev = document.getElementById('prev');
+const next = document.getElementById('next');
+const circles = document.querySelectorAll('circle');
+
+
+const currentActive = 1;
+next.addEventListener('click', () => {
+    currentActive++;
+    if(currentActive > circles.length){
+        currentActive = circles.length;
+    }
+    update();
+})
+
+prev.addEventListener('click', () => {
+    currentActive++;
+    if(currentActive < 1){
+        currentActive = 1;
+    }
+    update();
+})
+
+function update() {
+    
+}
