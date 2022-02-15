e27a717e01457ca549bfd4535911b347ae16d218
Miguel Lima <miguellimaoliver97@gmail.com>
2022-02-15 18:33:22 -0300

    Segunda opcao para o menu JS

--- menuDeNavegacao/script.js
+++ menuDeNavegacao/script.js
@@ -1,4 +1,5 @@
-function menuToggle() {
+// Primeira opção para realizar a abertura e fechamento do menu navegador
+/* function menuToggle() {
     let menuArea = document.getElementById("menu-area");
 
     if(menuArea.classList.contains('menu-opened') == true) {
@@ -6,5 +7,15 @@ function menuToggle() {
     } else {
         menuArea.classList.add('menu-opened');
     }
-    console.log(menuArea)
+    console.log(menuArea) 
+} */
+
+// Segunda opção para realizar a abertura e fechamento do menu navegador
+function menuToggle() {
+    let menuArea2 = document.getElementById("menu-area");
+    if(menuArea2.style.width == '200px') {
+        menuArea2.style.width = '0px';
+    } else {
+        menuArea2.style.width = '200px';
+    }
 }
