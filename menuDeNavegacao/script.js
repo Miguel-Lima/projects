f6a77bdb8a055683bdf794d183a3b8ad4c309e7d
Miguel Lima <miguellimaoliver97@gmail.com>
2022-02-15 18:29:33 -0300

    Menu area funcionando

--- menuDeNavegacao/script.js
+++ menuDeNavegacao/script.js
@@ -1,3 +1,10 @@
 function menuToggle() {
-    
+    let menuArea = document.getElementById("menu-area");
+
+    if(menuArea.classList.contains('menu-opened') == true) {
+        menuArea.classList.remove('menu-opened');
+    } else {
+        menuArea.classList.add('menu-opened');
+    }
+    console.log(menuArea)
 }
